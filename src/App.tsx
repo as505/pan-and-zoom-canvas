5b4549b1a1d29cd23c3de3ab63a89b47b85b8be0
Anders <as505soreide@gmail.com>
2025-06-06 01:18:04 +0200

    added zoom support

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,33 @@
 import canvas_image from '/Utah_teapot_simple_2.png'
 import './App.css'
+import { useState } from "react";
+import { useMemo } from 'react';
+
+
 
 function App() {
+  // Lets users zoom with scroll wheel
+  const [zoom, setZoom] = useState({z : 100})
+  const handleMouseMove = (e) => {
+    // deltaY is positive or negative depending on scroll direction
+    if (e.deltaY > 0){
+      setZoom({z: zoom.z - 2})
+    } else {
+      setZoom({z: zoom.z + 2})
+    }
+    
+    // Update zoom property
+    e.currentTarget.style.setProperty('--zoom', `${zoom.z}%`);
+  };
+
   return (
     <>
-      <div className='canvasFrame'>
+      <div className='canvasFrame' onWheel={handleMouseMove}>
           <img src={canvas_image} className="canvasImage" alt="Picture of the Utah Teapot"/>
       </div>
     </>
   )
 }
+
 
 export default App
